6e73d3ac2fb80e3ff24f724e6ef2a8c1358ce8d7
bhopinder <bhopinder92@gmail.com>
2024-02-28 20:39:45 +0530

    all file added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name="cencus-income",
+      version="0.0.1",
+      author="bhopinder",
+      author_email="bhopinder92@gmail.com",
+      packages=find_packages(),
+      install_requires=["pandas","numpy","flask"],
+      )
